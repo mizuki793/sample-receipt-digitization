cd819f4900a54744f5ec2ec4ba6fa7603c102779
mizuki <51521774+mizuki793@>
2026-05-19 17:59:22 +0900

    feat: #23- レビュー反映

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     GOOGLE_API_KEY: str = "mock-key-for-local-testing"
 
     model_config = SettingsConfigDict(
-        env_file="app/  .env", 
+        env_file="app/.env", 
         env_file_encoding="utf-8",
     )
 settings = Settings()
